2a67d3a37fb79fc2560c2c312f3375bf020c1434
valenti94br <valenti94br@gmail.com>
2023-11-06 20:49:09 +0100

    lógica ahí ahí

---
+++ main.js
@@ -0,0 +1,18 @@
+$(".flex-slice").each(function () {
+    $(this).hover(function ({
+        $(this).find('.flex-title').css({
+            transform: 'rotate(0deg)',
+            top: '10%'
+        });
+    $(this).find('.flex-about').css({
+        opacity: '1'
+    });
+}, function () {
+    $(this).find('.flex-title').css({
+        transform: 'rotate(90deg)',
+        top: '15%'
+    });
+$(this).find('.flex-about').css({
+    opacity: '0'
+});
+    });
